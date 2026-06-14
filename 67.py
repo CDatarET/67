1b2c3b86edb86032f31b96c10f148c2f1c77061d
CdatarET <datar.chaitanya@gmail.com>
2026-06-14 18:28:53 +0530

    67 hehe

---
+++ 67.py
@@ -0,0 +1,45 @@
+import curses
+import time
+import math
+
+six = ["███", "█", "████", "█   █", " ███"]
+seven = ["█████", "    █", "   █", "  █", " █"]
+
+def tri(t):
+    return 1 - abs((t % 2) - 1)
+
+def main(stdscr):
+    curses.curs_set(0)
+    stdscr.nodelay(True)
+
+    t = 0
+    max_offset = 4
+
+    while True:
+        stdscr.erase()
+
+        # constant-speed bounce (no easing slowdown)
+        base = tri(t * 0.2)
+
+        offset_six = int(base * max_offset)
+        offset_seven = max_offset - offset_six
+
+        six_lines = ([""] * offset_six) + six
+        seven_lines = ([""] * offset_seven) + seven
+
+        max_lines = max(len(six_lines), len(seven_lines))
+
+        for i in range(max_lines):
+            left = six_lines[i] if i < len(six_lines) else ""
+            right = seven_lines[i] if i < len(seven_lines) else ""
+            stdscr.addstr(i, 0, left.ljust(10) + "   " + right)
+
+        stdscr.refresh()
+        time.sleep(0.05)
+
+        t += 1
+
+        if stdscr.getch() != -1:
+            break
+
+curses.wrapper(main)
